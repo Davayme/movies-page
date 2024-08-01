3f70c42ad11339e89039693bc62c77f208630b70
David Ayme <dayme1937@uta.edu.ec>
2024-08-01 13:49:50 -0500

    chore: Update dependencies and add movie and start components

--- src/app/core/services/movies-service.service.ts
+++ src/app/core/services/movies-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Movie } from '../models/movie.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MoviesServiceService {
+export class MoviesService{
 
-  constructor() { }
+  private apiUrl = 'https://api.themoviedb.org/3/movie/popular?language=es-Es&page=1';
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Accept': 'application/json',
+      'Authorization': environment.apiToken
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getPopularMovies(): Observable<Movie> {
+    return this.http.get<Movie>(this.apiUrl, this.httpOptions);
+  }
 }
--- src/app/features/start/start.component.ts
+++ src/app/features/start/start.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/core/services/movies-service.service';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: []
 })
-export class StartComponent {
+export class StartComponent implements OnInit {
+
+  constructor(private _movieService : MoviesService) { }
+
+  ngOnInit(): void {
+    this._movieService.getPopularMovies().subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  } 
 
 }
