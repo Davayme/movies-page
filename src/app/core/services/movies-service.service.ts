00502926d3022ad2215533b6b6c500120e42a043
David Ayme <dayme1937@uta.edu.ec>
2024-08-01 21:25:09 -0500

    feat: Update movie service to retrieve movie details

--- src/app/core/services/movies-service.service.ts
+++ src/app/core/services/movies-service.service.ts
@@ -3,13 +3,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Movie } from '../models/movie.model';
+import { MovieDetail } from '../models/movieDetail';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService{
 
-  private apiUrl = 'https://api.themoviedb.org/3/movie/popular?language=es-Es';
+  private apiUrl = 'https://api.themoviedb.org/3/movie';
   private httpOptions = {
     headers: new HttpHeaders({
       'Accept': 'application/json',
@@ -19,7 +20,11 @@ export class MoviesService{
 
   constructor(private http: HttpClient) { }
 
-  getPopularMovies(page:number = 1): Observable<Movie> {
-    return this.http.get<Movie>(`${this.apiUrl}&page=${page}`, this.httpOptions);
+  getPopularMovies(page: number = 1): Observable<Movie> {
+    return this.http.get<Movie>(`${this.apiUrl}/popular?language=es-ES&page=${page}`, this.httpOptions);
+  }
+
+  getMovieById(id: number): Observable<MovieDetail> {
+    return this.http.get<MovieDetail>(`${this.apiUrl}/${id}?language=es-ES`, this.httpOptions);
   }
 }
